14d769d3abeed8a08364ccc5639eb7c4b94f0141
Cedric Kienzler <cedrickienzler1@gmail.com>
2017-02-15 07:14:00 +0100

    improved config and new i3exit script

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 # execute with sudo
 
-sudo cp lock.sh /bin/lock
-chmod +x /bin/lock
+sudo cp /home/c3d1/.i3/lock.sh /bin/lock
+chmod +x /usr/bin/lock
+
+ln -s /home/c3d1/.i3/i3exit.sh /usr/bin/i3exit 
+chmod +x /usr/bin/i3exit
