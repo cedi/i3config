d101fada56c369d02aaabbb7efaf0e4f69609f3b
cedi <cedi@users.noreply.github.com>
2017-04-26 12:29:39 +0200

    use symlink instead of copy

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # execute with sudo
 
-sudo cp /home/c3d1/.i3/lock.sh /bin/lock
+ln -s /home/c3d1/.i3/lock.sh /usr/bin/lock
 chmod +x /usr/bin/lock
 
 ln -s /home/c3d1/.i3/i3exit.sh /usr/bin/i3exit 
