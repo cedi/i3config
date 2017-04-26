84a91c9c1854662e6cf2138b2bf42d6e347e1fba
cedi <cedi@users.noreply.github.com>
2017-04-26 12:30:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # execute with sudo
 
-ln -s /home/c3d1/.i3/lock.sh /usr/bin/lock
+ln -s ~/.i3/lock.sh /usr/bin/lock
 chmod +x /usr/bin/lock
 
-ln -s /home/c3d1/.i3/i3exit.sh /usr/bin/i3exit 
+ln -s ~/.i3/i3exit.sh /usr/bin/i3exit 
 chmod +x /usr/bin/i3exit
