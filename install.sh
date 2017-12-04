239df69627272775825051b3b04b89c1f399baed
cedi <cedi@users.noreply.github.com>
2017-12-04 10:16:56 +0100

    fix the installer script

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 # execute with sudo
 
-ln -s ~/.i3/lock.sh /usr/bin/lock
-chmod +x /usr/bin/lock
+ln -s /home/cedi/.config/i3/lock.sh /usr/local/bin/lock
+chmod +x /usr/local/bin/lock
 
-ln -s ~/.i3/i3exit.sh /usr/bin/i3exit 
-chmod +x /usr/bin/i3exit
+ln -s /home/cedi/.config/i3/i3exit.sh /usr/local/bin/i3exit 
+chmod +x /usr/local/bin/i3exit
 
-ln -s ~/.i3/rofi-run.sh /usr/bin/rofi-run
-chmod +x /usr/bin/rofi-run
+ln -s /home/cedi/.config/i3/rofi-run.sh /usr/local/bin/rofi-run
+chmod +x /usr/local/bin/rofi-run
+
+ln -s /home/cedi/.config/i3/volume.sh /usr/local/bin/current_volume
+chmod +x /usr/local/bin/current_volume
