ba30c54359291b994ba605af3937ff81d4e19c8c
cedi <cedi@users.noreply.github.com>
2018-08-06 10:45:15 +0200

    remove theme...

--- rofi-run.sh
+++ rofi-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -combi-modi window,drun,ssh -show combi -modi combi -theme $HOME/src/rofi/themes/android_notification.rasi &
+rofi -combi-modi window,drun,ssh -show combi -modi combi -terminal tilix&
